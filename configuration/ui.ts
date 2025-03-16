061aa78a75434f3427fe770961f5f311bc1f2dd0
ianwhite03 <ianw03@business.unc.edu>
2025-03-16 19:48:39 -0400

    Update ui.ts
    Replaced AI Name and Owner Name with Sebastian and Ian

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,4 +1,4 @@
-import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
+import { Seb_the_Sports_Statistician, Ian } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
