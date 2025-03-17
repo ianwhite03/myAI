0cac21d41403a8bc02297e39b0596d1c1d3bdd85
ianwhite03 <ianw03@business.unc.edu>
2025-03-16 23:25:33 -0400

    Update ui.ts
    Updated clear chat comment

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = 'Marvel Universe Chatbot';
 export const MESSAGE_PLACEHOLDER: string = 'Ask me a Marvel question...';
 export const FOOTER_MESSAGE: string = 'Customized AI by Ian White and Tony Stark';
-export const CLEAR_BUTTON_TEXT: string = 'Clear Chat';
+export const CLEAR_BUTTON_TEXT: string = 'Snap like Thanos';
 export const PAGE_TITLE: string = 'Marvel Chatbot';
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
