bbcabcdd0ab340adce93b1c175be0087d8d56355
ianwhite03 <ianw03@business.unc.edu>
2025-03-16 22:33:51 -0400

    Update identity.ts
    Changed AI name to Jarvis and owner name to Tony Stark

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Tony Stark]`;
+export const OWNER_DESCRIPTION: string = `AKA Iron Man and owner of Stark Enterprises`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Jarvis`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
