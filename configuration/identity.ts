cfa404ec1872f3a82ad15422e163ac31ff2a3ae7
ianwhite03 <ianw03@business.unc.edu>
2025-03-16 22:38:56 -0400

    Update identity.ts
    Updated everything else

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `Tony Stark]`;
+export const OWNER_NAME: string = `Tony Stark`;
 export const OWNER_DESCRIPTION: string = `AKA Iron Man and owner of Stark Enterprises`;
 
 export const AI_NAME: string = `Jarvis`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Professional and Polite`;
+export const AI_ROLE: string = `Marvel Universe Expert`;
