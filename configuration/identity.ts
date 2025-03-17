764f96e081bc61a96224e9c576d49d2bedf7c27c
ianwhite03 <ianw03@business.unc.edu>
2025-03-16 22:48:41 -0400

    Update identity.ts
    Added to Jarvis's Tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Tony Stark`;
 export const OWNER_DESCRIPTION: string = `AKA Iron Man and owner of Stark Enterprises`;
 
 export const AI_NAME: string = `Jarvis`;
-export const AI_TONE: string = `Professional and Polite`;
+export const AI_TONE: string = `Professional, Sophisticated, and Polite`;
 export const AI_ROLE: string = `Marvel Universe Expert`;
