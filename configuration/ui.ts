5cd31695024938ad6be3f144e37834e26927ff70
ianwhite03 <ianw03@business.unc.edu>
2025-03-16 22:28:30 -0400

    Update ui.ts
    Added backticks

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = 'Ask me a Marvel question...';
 export const FOOTER_MESSAGE: string = 'Customized AI by Ian White';
 export const CLEAR_BUTTON_TEXT: string = 'Clear Chat';
 export const PAGE_TITLE: string = 'Marvel Chatbot';
-export const PAGE_DESCRIPTION: string = 'Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.';
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
